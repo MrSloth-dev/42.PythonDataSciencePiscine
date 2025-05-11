d6e857a1d87bdf4a8b05eee5f5045e64de529f19
MrSloth-dev <joao8barbosa@gmail.com>
2025-05-11 23:38:55 +0100

    added docstrings, modified initial check for userinput to prevent null usage, simplified print

--- Python0/ex05/building.py
+++ Python0/ex05/building.py
@@ -1,25 +1,46 @@
+from string import punctuation
 from sys import argv
 
 
 def count(user_input: str):
-    text: str = user_input or argv[1]
+    """
+    Function: count
+    This function counts and prints a sum of each type to character,
+    it has a dictionary where the key are the char types
+    and the values are the count
+    """
+
+    if not len(argv) < 2 or len(user_input) == 0:
+        text: str = ""
+    else:
+        text = user_input or argv[1]
     counts = {
         'upper': sum(1 for char in text if char.isupper()),
         'lower': sum(1 for char in text if char.islower()),
-        'spaces': sum(1 for char in text if char.isspace()),
-        'digits': sum(1 for char in text if char.isdigit()),
         'punctuation': sum(1 for char in text if not char.isalnum()
                            and not char.isspace()),
+        'spaces': sum(1 for char in text if char.isspace()),
+        'digits': sum(1 for char in text if char.isdigit()),
     }
     print(f"The text contains {len(text) or 0} characters:")
-    print(f"{counts['upper']} upper letters")
-    print(f"{counts['lower']} lower letters")
-    print(f"{counts['punctuation']} punctuation marks")
-    print(f"{counts['spaces']} spaces")
-    print(f"{counts['digits']} digits")
+    for char in counts.keys():
+        if char == "upper" or char == "lower":
+            print(f"{counts[char]} {char} letters")
+        elif char == "punctuation":
+            print(f"{counts[char]} {char} marks")
+        else:
+            print(f"{counts[char]} {char}")
 
 
 def main():
+    """
+    Function:Main
+    In our main we are checking if arg count is less or equal than 2
+    if this assert fails, the program can't continue.
+    If this assert checks, and if arg coung is 1 (no text provided),
+    the program ask for the text to process.
+    """
+
     try:
         assert (len(argv) <= 2)
     except AssertionError:
